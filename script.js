df993bded68595b2199f4e9e5b6b177f286b6dc2
umair-rao <umairmuqeem@gmail.com>
2022-10-11 15:50:00 +0500

    Error Remove

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-let menuButton = document.getElementById('menu-button');
-let menu = document.getElementById('menu-id');
-let cross = document.getElementById('cross-id');
-let links = document.getElementsByClassName('menu-link')
+// Elements accesed
+const menuButton = document.getElementById('menu-button');
+const menu = document.getElementById('menu-id');
+const cross = document.getElementById('cross-id');
+const links = document.getElementsByClassName('menu-link');
 
+// Function to Show menu
 function showMenu() {
   menu.classList.replace('hide', 'show');
 }
 
+// Function to Hide Menu
 function hideMenu() {
   menu.classList.replace('show', 'hide');
 }
 
+// EventListener to perform function
 menuButton.addEventListener('click', showMenu);
+
 cross.addEventListener('click', hideMenu);
 for (let i = 0; i < links.length; i++) {
   links[i].addEventListener('click', hideMenu);
